6d9dbc05e9cb0935315d689436929ef811b8b721
DESKTOP-H78JT4V\Douc <ablodouc@gmail.com>
2020-08-07 16:15:10 +0300

    07 august 2020

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class BlogController extends AbstractController
+{
+    public function index()
+    {
+        /*return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);*/
+        //return new Response('<h1>Page d\'accueil</h1>');
+        return $this->render('blog/index.html.twig');
+    }
+
+    public function add()
+    {
+    	return $this->render('blog/add.html.twig');
+    }
+
+    public function show($url)
+    {
+    	return $this->render('blog/show.html.twig', [
+            'slug' => $url
+        ]);
+    }
+
+    public function edit($id)
+    {
+    	return $this->render('blog/edit.html.twig', [
+            'slug' => $id
+        ]);
+    }
+
+    public function remove($id)
+    {
+    	return new Response('<h1>Supprimer l\'article ' .$id. '</h1>');
+    } 
+}
